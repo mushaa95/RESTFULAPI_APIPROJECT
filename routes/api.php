6c975c787c2e4e4468a66369cc8c4b03872675ff
Muslih <muslihuddin.abdulghani@gmail.com>
2024-06-30 22:06:55 +0800

    update route and appserviceprovider

--- routes/api.php
+++ routes/api.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::apiResource('blogs', BlogController::class)->middleware('auth:sanctum')->middleware(['throttle:user']);
+Route::apiResource('blogs', BlogController::class)->middleware(['throttle:user', 'auth:sanctum']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
